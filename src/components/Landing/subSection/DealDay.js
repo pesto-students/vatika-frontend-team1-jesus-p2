6bfbc6ea0bde0af962763c4c9f857e152c70a0b4
Kavish15 <kavishgarg15@gmail.com>
2022-10-20 03:05:21 +0530

    Added Timer in Deal of the Day Section

--- src/components/Landing/subSection/DealDay.js
+++ src/components/Landing/subSection/DealDay.js
@@ -1,19 +1,30 @@
 import React from "react";
 import Heading from "../../Common/Heading/Heading";
-import { Rate } from "antd";
+import { Rate, Statistic } from "antd";
 import "../css/DealDay.css";
 
 function DealDay() {
+  const { Countdown } = Statistic;
+  const deadline = Date.now() + 1000 * 60 * 60 * 24 ;
+
+  const onFinish = () => {
+    console.log("finished!");
+  };
+
   return (
     <div className="dealDay">
       <Heading content={"Today's Deal"} />
+      <Countdown value={deadline} className="countdown" onFinish={onFinish} />
       <div className="deal">
         <div className="left-plant">
           <h2>Limited Stock HurryUp!</h2>
         </div>
 
         <div className="right-plant r-1">
-          <img src="https://res.cloudinary.com/ds4gbbc6g/image/upload/v1665863256/Vatika/React%20FrontEnd/plant-1_akbp5s.png" alt="DealPlant1" />
+          <img
+            src="https://res.cloudinary.com/ds4gbbc6g/image/upload/v1665863256/Vatika/React%20FrontEnd/plant-1_akbp5s.png"
+            alt="DealPlant1"
+          />
           <h3>Money Plant</h3>
           <Rate
             defaultValue="4"
@@ -26,7 +37,10 @@ function DealDay() {
           </p>
         </div>
         <div className="right-plant r-2">
-          <img src="https://res.cloudinary.com/ds4gbbc6g/image/upload/v1665863256/Vatika/React%20FrontEnd/plant-1_akbp5s.png" alt="DealPlant1" />
+          <img
+            src="https://res.cloudinary.com/ds4gbbc6g/image/upload/v1665863256/Vatika/React%20FrontEnd/plant-1_akbp5s.png"
+            alt="DealPlant1"
+          />
           <h3>Money Plant</h3>
           <Rate
             defaultValue="4"
@@ -39,7 +53,10 @@ function DealDay() {
           </p>
         </div>
         <div className="right-plant r-3">
-          <img src="https://res.cloudinary.com/ds4gbbc6g/image/upload/v1665863256/Vatika/React%20FrontEnd/plant-1_akbp5s.png" alt="DealPlant1" />
+          <img
+            src="https://res.cloudinary.com/ds4gbbc6g/image/upload/v1665863256/Vatika/React%20FrontEnd/plant-1_akbp5s.png"
+            alt="DealPlant1"
+          />
           <h3>Money Plant</h3>
           <Rate
             defaultValue="4"
@@ -53,7 +70,10 @@ function DealDay() {
           </p>
         </div>
         <div className="right-plant r-4">
-          <img src="https://res.cloudinary.com/ds4gbbc6g/image/upload/v1665863256/Vatika/React%20FrontEnd/plant-1_akbp5s.png" alt="DealPlant1" />
+          <img
+            src="https://res.cloudinary.com/ds4gbbc6g/image/upload/v1665863256/Vatika/React%20FrontEnd/plant-1_akbp5s.png"
+            alt="DealPlant1"
+          />
           <h3>Money Plant</h3>
           <Rate
             defaultValue="4"
@@ -67,7 +87,10 @@ function DealDay() {
           </p>
         </div>
         <div className="right-plant r-5">
-          <img src="https://res.cloudinary.com/ds4gbbc6g/image/upload/v1665863256/Vatika/React%20FrontEnd/plant-1_akbp5s.png" alt="DealPlant1" />
+          <img
+            src="https://res.cloudinary.com/ds4gbbc6g/image/upload/v1665863256/Vatika/React%20FrontEnd/plant-1_akbp5s.png"
+            alt="DealPlant1"
+          />
           <h3>Money Plant</h3>
           <Rate
             defaultValue="4"
@@ -81,7 +104,10 @@ function DealDay() {
           </p>
         </div>
         <div className="right-plant r-6">
-          <img src="https://res.cloudinary.com/ds4gbbc6g/image/upload/v1665863256/Vatika/React%20FrontEnd/plant-1_akbp5s.png" alt="DealPlant1" />
+          <img
+            src="https://res.cloudinary.com/ds4gbbc6g/image/upload/v1665863256/Vatika/React%20FrontEnd/plant-1_akbp5s.png"
+            alt="DealPlant1"
+          />
           <h3>Money Plant</h3>
           <Rate
             defaultValue="4"
